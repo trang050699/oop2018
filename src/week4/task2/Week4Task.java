91757be119b7f280e4b6dde74225ffa84928ecd9
trang050699 <43141355+trang050699@users.noreply.github.com>
2018-10-12 16:51:17 +0700

    Add files via upload

---
+++ src/week4/task2/Week4Task.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.task;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Week4Task {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Circle circle0 = new Circle(1.0);
+        
+    }
+    
+}
