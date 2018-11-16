b10a5dad82044089e78419dfdf54c61996fe2986
trang050699 <43141355+trang050699@users.noreply.github.com>
2018-11-16 15:25:59 +0700

    up

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,34 @@
 package week10;
 
+
+import java.util.Random;
+
+
 public class Task2 {
+    public static void main(String[] args){
+        Random s = new Random();
+        int[] a = new int[100];
+        for(int i=0;i<100;i++){
+            a[i] = s.nextInt();
+        }
+        System.out.println("Day so ban dau:");
+        for(int i=0; i<100; i++){
+            System.out.print(a[i]+" ");
+        }
+        int x;
+        for(int i=0;i<100;i++){
+            for(int y=0;y<100-i;y++){
+                if(a[y]>a[y+1]){
+                    x=a[y];
+                    a[y]=a[y+1];
+                    a[y+1]=x;
+                }
+            }
+        }
+        System.out.println("Day so sau khi sap xep:");
+        for(int i=0;i<100;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+    
 }
