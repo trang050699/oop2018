8e170880ec00a58f5a1c2556d7236181aa00e9e6
trang050699 <43141355+trang050699@users.noreply.github.com>
2018-11-23 16:38:58 +0700

    up

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,20 @@
 package week11;
 
 public class Task2 {
+  public static <T extends Comparable<T>> T LN(ArrayList<T> a){
+        T x=a.get(0);
+        for(int i=0;i<a.size();i++){
+            if(a.get(i).compareTo(x)>0){
+                x= a.get(i);
+            }
+        }
+        return x;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList();
+        a.add(2);
+        a.add(6);
+        a.add(0);
+        System.out.print("Phan tu lon nhat:"+LN(a));
+    }
 }
